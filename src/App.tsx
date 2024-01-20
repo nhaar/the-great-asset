47eb5662930010b4edd144460275b968bebe7f99
nhaar <38634785+nhaar@users.noreply.github.com>
2024-01-20 19:53:10 -0300

    add quota calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import QuotaCalculator from './QuotaCalculator'
 
 export default function App (): JSX.Element {
+  const match: RegExpMatchArray = window.location.pathname.match(/\/the-great-asset\/(.*)/) as RegExpMatchArray
+  
+
+  const path: string = match[1]
+  if (path == 'quota') {
+    return <QuotaCalculator />
+  }
   return (
     <div>
       Hello Company!
---
+++ src/QuotaCalculator.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+export default function QuotaCalculator(): JSX.Element {
+  const [timesFulfilled, setTimesFulfilled] = useState<string>('0')
+  const [previousQuota, setPreviousQuota] = useState<number>(130)
+
+  function updateTimesFulfilled(event: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(event.target.value)
+    setTimesFulfilled(event.target.value)
+  }
+
+  function updatePreviousQuota(event: React.ChangeEvent<HTMLInputElement>) : void {
+    setPreviousQuota(Number(event.target.value))
+  }
+
+  function getNewProfitQuota(randomValue: number): number {
+    // the result is truncated in the end in-game
+    const times: number = Number(timesFulfilled)
+    return Math.floor(previousQuota + 100 * (1 + Math.pow(times, 2) / 16) * (randomValue + 1))
+  }
+  
+  return (
+    <div>
+      <div>
+        This calculator will calculate the possible new profit quotas.
+      </div>
+      <div>
+        <div>
+          Previous profit quota
+        </div>
+        <input onChange={updatePreviousQuota} type="number" value={previousQuota} />
+      </div>
+      <div>
+        <div>
+          How many times the quota has been fulfilled before.
+        </div>
+        <input onChange={updateTimesFulfilled} type="number" value={timesFulfilled} />
+      </div>
+      <div>
+        The smallest possible new profit quota is {getNewProfitQuota(-0.5)}.
+      </div>
+      <div>
+        The average possible new profit quota is {getNewProfitQuota(0)}.
+      </div>
+      <div>
+        The biggest possible new profit quota is {getNewProfitQuota(0.5)}.
+      </div>
+    </div>
+  )
+}
