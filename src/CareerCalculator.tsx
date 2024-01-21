ba4b2cc6e0382a1e4ad0fd81929701f77b43d77b
nhaar <38634785+nhaar@users.noreply.github.com>
2024-01-21 11:28:25 -0300

    more features to run calculator

--- src/CareerCalculator.tsx
+++ src/CareerCalculator.tsx
@@ -7,6 +7,7 @@ interface RunData {
 
 interface TimeData {
   days: DaysData[]
+  currentQuota: number
   scrapSold: number
 }
 
@@ -42,6 +43,11 @@ function isTimeData (obj: any): obj is TimeData {
   } else if (typeof obj.scrapSold !== 'number') {
     return false
   }
+  if (obj.currentQuota === undefined) {
+    return false
+  } else if (typeof obj.currentQuota !== 'number') {
+    return false
+  }
   return true
 }
 
@@ -66,6 +72,9 @@ function isRunData (obj: any): obj is RunData {
 
 function QuotaTime ({ data, quotaNumber, setterFn }: { data: TimeData, quotaNumber: number, setterFn: (prev: any) => any }): JSX.Element {
   const [acquiredValues, setAcquiredValues] = useState<string[]>(['0', '0', '0'])
+  const [profitQuota, setProfitQuota] = useState<string>(String(data.currentQuota))
+  const [scrapSold, setScrapSold] = useState<string>(String(data.scrapSold))
+
   useEffect(() => {
     setterFn((prev: any) => {
       const newRunData = { ...prev }
@@ -78,12 +87,36 @@ function QuotaTime ({ data, quotaNumber, setterFn }: { data: TimeData, quotaNumb
     })
   }, [acquiredValues])
 
+  useEffect(() => {
+    setterFn((prev: any) => {
+      const newRunData = { ...prev }
+      newRunData.timeData[quotaNumber - 1].currentQuota = Number(profitQuota)
+      return newRunData
+    })
+  }, [profitQuota])
+
+  useEffect(() => {
+    setterFn((prev: any) => {
+      const newRunData = { ...prev }
+      newRunData.timeData[quotaNumber - 1].scrapSold = Number(scrapSold)
+      return newRunData
+    })
+  }, [scrapSold])
+
   function getUpdateAcquiredValue (index: number): (event: React.ChangeEvent<HTMLInputElement>) => void {
     return (event: React.ChangeEvent<HTMLInputElement>): void => {
       const newAcquiredValues = [...acquiredValues]
       newAcquiredValues[index] = event.target.value
       setAcquiredValues(newAcquiredValues)
     }
+  }
+
+  function updateProfitQuota (event: React.ChangeEvent<HTMLInputElement>): void {
+    setProfitQuota(event.target.value)
+  }
+
+  function updateScrapSold (event: React.ChangeEvent<HTMLInputElement>): void {
+    setScrapSold(event.target.value)
   }
 
   const days = [0, 1, 2].map((dayIndex) => {
@@ -102,11 +135,16 @@ function QuotaTime ({ data, quotaNumber, setterFn }: { data: TimeData, quotaNumb
       <div>
         QUOTA {quotaNumber}
       </div>
+      <input type='text' value={profitQuota} onChange={updateProfitQuota} />
       <div>
         <div>DAY</div>
         <div>ACQUIRED LOOT</div>
       </div>
       {days}
+      <div>
+        <div>SCRAP SOLD</div>
+        <input type='text' value={scrapSold} onChange={updateScrapSold} />
+      </div>
     </div>
   )
 }
@@ -129,7 +167,21 @@ export default function CareerCalculator (): JSX.Element {
     }
   })
 
-  function addQuota (): void {
+  const [nextQuotaDiv, setNextQuotaDiv] = useState<JSX.Element>(<div />)
+
+  function getShipTotal (): number {
+    let total: number = 0
+    for (const timeData of runData.timeData) {
+      for (const dayData of timeData.days) {
+        total += dayData.acquired
+      }
+      total -= timeData.scrapSold
+    }
+
+    return total + runData.shipCorrection
+  }
+
+  function addQuota (profitQuota: number): void {
     const newRunData = { ...runData }
     newRunData.timeData.push({
       days: [{
@@ -139,22 +191,55 @@ export default function CareerCalculator (): JSX.Element {
       }, {
         acquired: 0
       }],
+      currentQuota: profitQuota,
       scrapSold: 0
     })
     setRunData(newRunData)
   }
 
   useEffect(() => {
-    console.log(runData.timeData.length)
-    if (runData.timeData.length === 0 || runData.timeData[runData.timeData.length - 1].days.every(day => {
-      return day.acquired > 0
-    })) {
-      addQuota()
+    if (runData.timeData.length === 0) {
+      addQuota(130)
+    }
+    console.log(runData.timeData)
+    if (runData.timeData[runData.timeData.length - 1].scrapSold <= 0) {
+      setNextQuotaDiv((
+        <div>
+          Waiting to sell scrap...
+        </div>
+      ))
+    } else if (getShipTotal() > 0) {
+      setNextQuotaDiv((
+        <div>
+          You have enough to meet the next profit quota!
+        </div>
+      ))
+    } else {
+      setNextQuotaDiv((
+        <div>
+          <div>
+            As you have not met the profit quota, your performance has been deemed below standard. Welcome to our disciplinary process.
+          </div>
+          <div>
+            You survived up to quota {runData.timeData.length}.
+          </div>
+        </div>))
     }
   }, [runData])
 
   function saveRunData (): void {
     localStorage.setItem('the-great-asset', JSON.stringify(runData))
+  }
+
+  function fixShipTotal (): void {
+    const newTotal: string | null = window.prompt('What is the ship total?')
+    if (newTotal === null || newTotal.match(/^\d+$/) === null) {
+      window.alert('Invalid value')
+      return
+    }
+    const newRunData = { ...runData }
+    newRunData.shipCorrection = Number(newTotal) - getShipTotal()
+    setRunData(newRunData)
   }
 
   return (
@@ -163,6 +248,21 @@ export default function CareerCalculator (): JSX.Element {
         {runData.timeData.map((timeData, index) => {
           return <QuotaTime key={index} data={timeData} quotaNumber={index + 1} setterFn={setRunData} />
         })}
+      </div>
+      {nextQuotaDiv}
+      <div>
+        <div>
+          SHIP TOTAL
+        </div>
+        <div>
+          {getShipTotal()}
+        </div>
+        <div>
+          Ship total not right? Click the button to fix it.
+        </div>
+        <button onClick={fixShipTotal}>
+          FIX SHIP TOTAL
+        </button>
       </div>
     </div>
   )
